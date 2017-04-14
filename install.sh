57a7df8544be8888b0bef9c072ae2a00b423b494
root <root@localhost.localdomain>
2017-04-14 04:18:51 -0700

    20170414 - update install processes

---
+++ install.sh
@@ -0,0 +1,9 @@
+npm install gulp -g
+npm install bower -g  
+npm config set registry https://registry.npm.taobao.org
+npm install node-sass --registry=https://registry.npm.taobao.org
+npm install gulp-sass --registry=https://registry.npm.taobao.org
+npm install
+bower install --allow-root
+
+
