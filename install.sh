4511a2fa7cc15fef4cb70c2a9fe7fd42bedf7975
leon <guozb@msn.com>
2017-05-01 15:31:55 +0000

    update install.sh 1st

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
+cd ~/CSMP_FE
+. /etc/profile
+
+echo "sass_binary_site=https://npm.taobao.org/mirrors/node-sass/" > ~/.npmrc
+echo "phantomjs_cdnurl=https://npm.taobao.org/mirrors/phantomjs/" >> ~/.npmrc
+echo "electron_mirror=https://npm.taobao.org/mirrors/electron/" >> ~/.npmrc
+echo "registry=https://registry.npm.taobao.org" >> ~/.npmrc
+##npm config set registry https://registry.npm.taobao.org
+
+
 npm install gulp -g
-npm install bower -g  
-npm config set registry https://registry.npm.taobao.org
-npm install node-sass --registry=https://registry.npm.taobao.org
-npm install gulp-sass --registry=https://registry.npm.taobao.org
-npm install
+npm install bower -g
+npm install --unsafe-perm node-sass
+npm install --unsafe-perm gulp-sass
 bower install --allow-root
+npm install
+
 
 
