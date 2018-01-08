090fdd7a8c46bf95fa37b330c48ac34311ecd66b
guozb <guozb@msn.com>
2018-01-08 22:44:55 +0800

    dashboard for performantce v2

--- src/app/pages/dashboard/performanceByArrayDirector/performanceByArrayDirector.js
+++ src/app/pages/dashboard/performanceByArrayDirector/performanceByArrayDirector.js
@@ -25,9 +25,11 @@
             $scope.director_da = response.BackEnd.CurrentUtilization;
             $scope.director_rf = response.RDF.CurrentUtilization;
             $scope.graphs_fa = [];
- 
-             angular.forEach($scope.director_rf, function(item, index){
-
+            $scope.graphs_da = [];
+            $scope.graphs_rf = [];
+    
+            for ( var j in $scope.director_fa ) {
+                var item = $scope.director_fa[j];
 
                     for ( var key in item ) {
                         if ( key == 'DT' ) continue;
@@ -43,26 +45,87 @@
                         if ( isFind == false ) {
                             var graphsItem = {}; 
                             graphsItem["id"] =  key;
-                            graphsItem["balloonText"] =  "Utilization:<b>[[value]]</b>", 
+                            graphsItem["balloonText"] =  key + "<br><b>[[value]]</b>", 
                             graphsItem["bullet"] =  "round",
-                            graphsItem["bulletSize"] =  8,
-                            graphsItem["lineColor"] =  "#d1655d",
+                            graphsItem["bulletSize"] =  4,
+                            graphsItem["lineColor"] =  "#FF4500",
                             graphsItem["lineThickness"] = 2;
-                            graphsItem["negativeLineColor"] =  "#637bb6",
+                            graphsItem["negativeLineColor"] =  "#00FF00",
+                            graphsItem["negativeBase"] = 30,
                             graphsItem["type"] =  "smoothedLine",
                             graphsItem["valueField"] =  key,
- 
- console.log(key);
+  
                             $scope.graphs_fa.push(graphsItem);                   
                         }
+                } 
+            };
+ 
+    
+            for ( var j in $scope.director_da ) {
+                var item = $scope.director_da[j];
 
-     
-                    } 
-            }); 
+                    for ( var key in item ) {
+                        if ( key == 'DT' ) continue;
 
-            console.log($scope.graphs_fa);
+                        var isFind = false;
+                        for ( var i in $scope.graphs_da ) {
+                            var gItem = $scope.graphs_da[i];
+                            if ( gItem.id == key ) {
+                                isFind = true;
+                                break;
+                            }
+                        }
+                        if ( isFind == false ) {
+                            var graphsItem = {}; 
+                            graphsItem["id"] =  key;
+                            graphsItem["balloonText"] =  key + "<br><b>[[value]]</b>", 
+                            graphsItem["bullet"] =  "round",
+                            graphsItem["bulletSize"] =  4,
+                            graphsItem["lineColor"] =  "#FF4500",
+                            graphsItem["lineThickness"] = 2;
+                            graphsItem["negativeLineColor"] =  "#00FF00",
+                            graphsItem["negativeBase"] = 30,
+                            graphsItem["type"] =  "smoothedLine",
+                            graphsItem["valueField"] =  key,
+  
+                            $scope.graphs_da.push(graphsItem);                   
+                        }
+                } 
+            };
  
 
+            for ( var j in $scope.director_rf ) {
+                var item = $scope.director_rf[j];
+
+                    for ( var key in item ) {
+                        if ( key == 'DT' ) continue;
+
+                        var isFind = false;
+                        for ( var i in $scope.graphs_rf ) {
+                            var gItem = $scope.graphs_rf[i];
+                            if ( gItem.id == key ) {
+                                isFind = true;
+                                break;
+                            }
+                        }
+                        if ( isFind == false ) {
+                            var graphsItem = {}; 
+                            graphsItem["id"] =  key;
+                            graphsItem["balloonText"] =  key + "<br><b>[[value]]</b>", 
+                            graphsItem["bullet"] =  "round",
+                            graphsItem["bulletSize"] =  4,
+                            graphsItem["lineColor"] =  "#FF4500",
+                            graphsItem["lineThickness"] = 2;
+                            graphsItem["negativeLineColor"] =  "#00FF00",
+                            graphsItem["negativeBase"] = 30,
+                            graphsItem["type"] =  "smoothedLine",
+                            graphsItem["valueField"] =  key,
+  
+                            $scope.graphs_rf.push(graphsItem);                   
+                        }
+                } 
+            };
+ 
             var chart = AmCharts.makeChart("director_FA_chart", {
                 "type": "serial",
                 "theme": "black",
@@ -71,30 +134,8 @@
                 "color": "#E1DADA",  
                 "marginTop":0,
                 "marginRight": 0,
-                "dataProvider": [{
-        "year": "1966",
-        "value": -0.147
-    }, {
-        "year": "1967",
-        "value": -0.15
-    }, {
-        "year": "1968",
-        "value": -0.16
-    }, {
-        "year": "1969",
-        "value": -0.011
-    }],
-                "graphs": [{
-        "id":"g1",
-        "balloonText": "[[category]]<br><b><span style='font-size:14px;'>[[value]]</span></b>",
-        "bullet": "round",
-        "bulletSize": 8,
-        "lineColor": "#d1655d",
-        "lineThickness": 2,
-        "negativeLineColor": "#637bb6",
-        "type": "smoothedLine",
-        "valueField": "value"
-    }],
+                "dataProvider": $scope.director_fa,
+                "graphs": $scope.graphs_fa,
                 "chartCursor": { 
                     "cursorAlpha": 0,
                     "valueLineEnabled":true,
@@ -102,9 +143,9 @@
                     "valueLineAlpha":0.5,
                     "fullWidth":true
                 }, 
-                "categoryField": "year",
+                "categoryField": "DT",
                 "categoryAxis": { 
-                    "parseDates": true,
+                    "parseDates": false,
                     "minorGridAlpha": 0.1,
                     "minorGridEnabled": true
                 },
@@ -114,6 +155,67 @@
             });
 
  
+            var chart = AmCharts.makeChart("director_DA_chart", {
+                "type": "serial",
+                "theme": "black",
+                "plotAreaBorderColor": "#FF0000", 
+                "plotAreaFillColors": "#383535",    
+                "color": "#E1DADA",  
+                "marginTop":0,
+                "marginRight": 0,
+                "dataProvider": $scope.director_da,
+                "graphs": $scope.graphs_da,
+                "chartCursor": { 
+                    "cursorAlpha": 0,
+                    "valueLineEnabled":true,
+                    "valueLineBalloonEnabled":true,
+                    "valueLineAlpha":0.5,
+                    "fullWidth":true
+                }, 
+                "categoryField": "DT",
+                "categoryAxis": { 
+                    "parseDates": false,
+                    "minorGridAlpha": 0.1,
+                    "minorGridEnabled": true
+                },
+                "export": {
+                    "enabled": true
+                }
+            });
+
+ 
+
+ 
+            var chart = AmCharts.makeChart("director_RF_chart", {
+                "type": "serial",
+                "theme": "black",
+                "plotAreaBorderColor": "#FF0000", 
+                "plotAreaFillColors": "#383535",    
+                "color": "#E1DADA",  
+                "marginTop":0,
+                "marginRight": 0,
+                "dataProvider": $scope.director_rf,
+                "graphs": $scope.graphs_rf,
+                "chartCursor": { 
+                    "cursorAlpha": 0,
+                    "valueLineEnabled":true,
+                    "valueLineBalloonEnabled":true,
+                    "valueLineAlpha":0.5,
+                    "fullWidth":true
+                }, 
+                "categoryField": "DT",
+                "categoryAxis": { 
+                    "parseDates": false,
+                    "minorGridAlpha": 0.1,
+                    "minorGridEnabled": true
+                },
+                "export": {
+                    "enabled": true
+                }
+            });
+
+ 
+  
 
     }); 
  
